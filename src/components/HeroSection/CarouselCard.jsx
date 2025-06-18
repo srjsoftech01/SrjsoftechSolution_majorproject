c5bf0f15334bcd54a013ed3f9477daaef60775af
Arvind Kumar <arvindkumar735240@gmail.com>
2025-06-18 15:06:43 +0530

    backend server added

--- src/components/HeroSection/CarouselCard.jsx
+++ src/components/HeroSection/CarouselCard.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const CarouselCard = ({ image, title }) => {
   return (
-    <div className="bg-black text-white rounded-2xl shadow-lg p-4 flex flex-col items-center text-center min-h-[300px]">
+    <div className=" bg-gradient-to-r from-green-300 to-blue-500  text-white rounded-2xl shadow-lg p-4 flex flex-col items-center text-center min-h-[300px]">
       <img src={image} alt={title} className="w-full h-48 object-contain rounded-md" />
       <h3 className="mt-4 text-xl font-bold">{title}</h3>
     </div>
